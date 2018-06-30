36a80864ddad46c776ce07b988f7c2b5c63dd366
Vishal <vishalpuppala@gmail.com>
2018-06-30 18:57:07 +0530

    commint

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-  document.getElementById('heading').innerHTML = localStorage['title'] || 
-  document.getElementById('content').innerHTML = localStorage['text'] || 'Your text here...'; // default text
+  document.getElementById('heading').innerHTML = localStorage['title'] 
+  document.getElementById('content').innerHTML = localStorage['text'] 
 
   setInterval(function() { // fuction that is saving the innerHTML of the div
 	localStorage['title'] = document.getElementById('heading').innerHTML; // heading div
